64763e08806923cae02465b2f22402d773c398ee
Aya Morisawa <AyaMorisawa4869@gmail.com>
2015-11-16 20:01:05 +0900

    Add Math.randomInteger

--- src/math.ts
+++ src/math.ts
@@ -1,3 +1,5 @@
+import Task from './task';
+
 export interface Point {
 	x: number;
 	y: number;
@@ -6,4 +8,9 @@ export interface Point {
 export function distance(point1: Point, point2: Point): number {
 	'use strict';
 	return Math.sqrt(Math.pow(point1.x - point2.x, 2) + Math.pow(point1.y - point2.y, 2));
+}
+
+export function randomInteger(min: number, max: number): Task<number> {
+	'use strict';
+	return Task.resolve(min + Math.floor(Math.random() * (max - min + 1)));
 }
