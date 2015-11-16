be979db18a89d5f849907ec09a93bad4115093ee
Aya Morisawa <AyaMorisawa4869@gmail.com>
2015-11-16 23:02:29 +0900

    Add Object.fromJson

--- src/object.ts
+++ src/object.ts
@@ -1,7 +1,18 @@
+import Either from './either';
+
 export function merge<T, U>(source1: T, source2: U): T & U;
 export function merge<T, U, V>(source1: T, source2: U, source3: V): T & U & V;
 export function merge<T, U, V, W>(source1: T, source2: U, source3: V, source4: W): T & U & V & W;
 export function merge(...sources: any[]): any {
 	'use strict';
 	return Object.assign({}, ...sources);
+}
+
+export function fromJson<T>(text: string): Either<SyntaxError, T> {
+	'use strict';
+	try {
+		return Either.right<SyntaxError, T>(JSON.parse(text));
+	} catch (e) {
+		return Either.left<SyntaxError, T>(e);
+	}
 }
