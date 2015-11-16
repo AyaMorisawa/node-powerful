f8bf68eccb7d9c818e02bc4cc6b171796b59ef47
Aya Morisawa <AyaMorisawa4869@gmail.com>
2015-11-16 23:02:01 +0900

    Fix the conflict with Object

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,11 @@
 import Task from './task';
 import Either from './either';
 import Option from './option';
-import * as Object from './object'
+import * as _Object from './object';
 import * as List from './list';
 import * as Math from './math';
 import * as String from './string';
 import id from './id';
 import print from './print';
 
-export { Task, Either, Option, Object, List, Math, String, id, print }
+export { Task, Either, Option, _Object as Object, List, Math, String, id, print }
